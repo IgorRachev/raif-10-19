826f767c58633cccb83e06afb3bb6d927bc255a5
galy4 <35988850+galy4@users.noreply.github.com>
2019-10-14 22:07:28 +0600

    Add Calculator class for testing

---
+++ HomeWork/Calculator.java
@@ -0,0 +1,27 @@
+package com.raif.cources.unitTestExample.calculator;
+
+public class Calculator {
+
+
+        public int sum(int a, int b) {
+            return a + b;
+        }
+
+        public int minus(int a, int b) {
+            return a + b;  // ERROR!!!
+        }
+
+        public int divide(int a, int b) {
+            return a / b;
+        }
+
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+
+        public double squareRoot(double n){
+            return Math.sqrt(n);
+        }
+
+}
+
